3c37146881f787f7537170825364f5968b8b11c8
OleksiiKostenko <kostenko.oleksii91@gmail.com>
2023-01-17 16:47:22 +0200

    very good

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,45 +1,55 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-// function createImgGallery() {
- 
-// };
 
-const imeges = galleryItems.map(item => {
+const takeDivTag = document.querySelector("div.gallery");
+
+function createImgGallery  (item)  {
+    return item.map(item => {
     
-    const takeDivTag = document.querySelector("div.gallery");
-    const divEl = document.createElement("div");
-    divEl.classList.add("gallery__item");
+        const divEl = document.createElement("div");
+        divEl.classList.add("gallery__item");
 
-    const linkEL = document.createElement("a");
-    linkEL.addEventListener('click', (evt) => {
-        evt.preventDefault();
-    }
-    )
-    linkEL.classList.add("gallery__link");
-    linkEL.href = "large-image.jpg";
+        const linkEL = document.createElement("a");
+        linkEL.addEventListener('click', (evt) => {
+            evt.preventDefault();
+            const instance = basicLightbox.create(`
+            <img src=${item.original} width="800" height="600">`)
+            instance.show()
+        });
+        linkEL.classList.add("gallery__link");
+        linkEL.href = item.original;
 
-    const imgEl = document.createElement("img");
-    imgEl.classList.add("gallery__image");
-    imgEl.src = item.preview;
-    imgEl.dataset.source = "large-image.jpg";
-    imgEl.alt = "Image description";
+
+      
+
+        const imgEl = document.createElement("img");
+        imgEl.classList.add("gallery__image");
+        imgEl.src = item.preview;
+        imgEl.dataset.source = item.original;
+        imgEl.alt = item.description;
 
    
     
-    linkEL.appendChild(imgEl);
-    divEl.appendChild(linkEL);
-    takeDivTag.appendChild(divEl);
+        linkEL.appendChild(imgEl);
+        divEl.appendChild(linkEL);
+        takeDivTag.appendChild(divEl);
 
-    return divEl;
-})
-
-
-
+        return divEl;
+    });
+      };
 
 
-console.log(imeges)
-console.log(takeDivTag);
+const element = createImgGallery(galleryItems);
+takeDivTag.append(...element);
+
+
+takeDivTag.addEventListener('click', () => {
+});
+
+
+
+
 console.log(galleryItems);
 
 
